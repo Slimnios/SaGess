182192b8cfd6e949baba0cce43edc6450907ccca
Praveen Selvaraj <praveen.beatle@gmail.com>
2024-01-30 19:42:06 +0000

    conda env setup

--- install_conda_env.sh
+++ install_conda_env.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CONDA_ENV=sagess
+# CUDA_VER={cu113}
+
 conda env create -f environment.yml
 
-source activate sagess 
+# Install additional Python packages with pip
+conda run -n $CONDA_ENV pip install torch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0
+conda run -n $CONDA_ENV pip install -r pyg_reqs.txt
 
-pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
-
-pip install -r pyg_reqs.txt -f https://pytorch-geometric.com/whl/torch-1.11.0+cu113.html
-
-conda clean -y --all
+# Clean up any unnecessary files
+conda run -n $CONDA_ENV conda clean -y --all
 
